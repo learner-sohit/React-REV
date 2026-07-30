90c3a7274e98d566749cadd9147adc8abc97fb9d
sohit kumar <sohitkumar9548@gmail.com>
2026-07-30 18:20:15 +0530

    components completed

---
+++ 02-components/src/App.jsx
@@ -0,0 +1,13 @@
+import Card from "./components/Card"
+import Navbar from "./components/Navbar";
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Card />
+    </>
+  );
+}
+
+export default App
---
+++ 02-components/src/components/Card.jsx
@@ -0,0 +1,13 @@
+const Card = () => {
+  return (
+    <div className="card">
+      <h1>Sohit Kumar</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium,
+        nulla!
+      </p>
+    </div>
+  );
+}
+
+export default Card
