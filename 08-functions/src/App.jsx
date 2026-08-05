6e53aafcb3a3bcfdc077c362428674871ef230a6
sohit kumar <sohitkumar9548@gmail.com>
2026-08-05 13:54:11 +0530

    functions completed

---
+++ 08-functions/src/App.jsx
@@ -0,0 +1,18 @@
+const App = () => {
+  return <div>App</div>;
+};
+
+export default App;
+
+// const App = () => {
+//   const handleChange = (e) => {
+//     console.log(e.target.value);
+//   };
+//   return (
+//     <div className="input">
+//       <input onChange={handleChange} type="text" />
+//     </div>
+//   );
+// };
+
+// export default App;
